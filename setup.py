6ab852433fc91a1c4415e74090e4a28a776bdfb5
Derek <34669580+deetungsten@users.noreply.github.com>
2020-12-06 16:06:46 -0800

    Update setup.py
    Removed numpy version limitation

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup(name='stable_baselines',
           'joblib',
           'cloudpickle>=0.5.5',
           'opencv-python',
-          'numpy<1.19.0',
+          'numpy',
           'pandas',
           'matplotlib'
       ] + find_tf_dependency(),
